5a2d4f7226805b6a5ba45b10bb0db411b9afb8db
Paul Belanger <pabelanger@redhat.com>
2019-01-28 11:14:13 -0500

    Initial commit
    First commit for borgbackup ansible role.
    
    Change-Id: I6b07d5c89d0aba62d361dbe103969ce14d1047b7
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ molecule/tests/test_role.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def test_backup_user(host):
+    user = host.user('borgbackup')
+    assert user.exists
+    assert user.name == 'borgbackup'
+    assert user.group == 'borgbackup'
+    assert user.home == '/var/lib/borgbackup'
+
+    f = host.file('/var/lib/borgbackup')
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'borgbackup'
+    assert f.group == 'borgbackup'
+    assert f.mode == 0o755
